116a8578cdfc63e99889307d22e4fd07776299b2
vickychappuis <svchappuis@gmail.com>
2026-05-21 13:59:07 -0300

    feat: add Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './styles/global.css'
 import './styles/textures.css'
 import { App } from './components/App'
@@ -7,5 +8,6 @@ import { App } from './components/App'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
